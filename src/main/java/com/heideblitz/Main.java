618ecca3d675f51397536d3b6834f0d2fa3d35ed
Nico Zimmermann <mail@nico-zimmermann.com>
2013-10-21 15:00:39 +0000

    git-svn-id: https://www.nico-zimmermann.com/src/tools/trunk/image-sorted-name@11 3ccb2cef-97d5-4316-a41d-74a68c222445

--- src/main/java/com/heideblitz/Main.java
+++ src/main/java/com/heideblitz/Main.java
@@ -17,10 +17,12 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.exif.ExifIFD0Directory;
 
 public class Main {
-	public static void main(String[] args) throws ImageProcessingException, IOException {
+	public static void main(String[] args) throws Throwable {
 		final Map<File, String> extMap = new HashMap<File, String>();
+		final Map<File, File> tempFileMap = new HashMap<File, File>();
 
 		List<File> files = new ArrayList<File>();
+		try{
 		for (File file : new File(args[0]).listFiles()) {
 			if (file.getName().startsWith(".")) {
 				continue;
@@ -28,6 +30,7 @@ public class Main {
 			File newFile = File.createTempFile(file.getName() + "__", "", file.getParentFile());
 			files.add(newFile);
 			file.renameTo(newFile);
+			tempFileMap.put(newFile, file);
 
 			String name = file.getName();
 			int idx = name.lastIndexOf('.');
@@ -58,6 +61,14 @@ public class Main {
 			File newFile = new File(file.getParent(), String.format("%04d", n) + (ext == null ? "" : "." + ext));
 			System.out.println("'" + file.getName() + "' -> '" + newFile.getName() + "'");
 			file.renameTo(newFile);
+			tempFileMap.remove(file);
+		}
+
+		}catch(Throwable e){
+		    for(Map.Entry<File,File> entry : tempFileMap.entrySet()){
+			entry.getKey().renameTo(entry.getValue());
+		    }
+		    throw e;
 		}
 	}
 }
