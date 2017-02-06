af2b2573953d549b35e815f0b120e1f05d6bb3ff
Nico Zimmermann <nico.zimmermann@iomedico.com>
2017-02-06 13:17:20 +0100

    better cygwin scripts

--- photo-rename.sh
+++ photo-rename.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
-wd=`pwd`/photo-rename
-
-cd "$1"
-dir=`pwd`
-
-cd "$wd"
-cd `dirname "$0"`
+dir=$(realpath "$1")
 
 echo "scanning files in: $dir"
+
+if which cygpath 2>/dev/null; then 
+    dir=$(cygpath -aw "$dir")
+fi
+
+cd "`dirname "$0"`/photo-rename"
 mvn compile exec:java -Dexec.args="'$dir' '$2'"
