354fe9b6e0e009e182f8f38fe66f500bd074d2f4
Nico Zimmermann <mail@nico-zimmermann.com>
2017-03-13 17:11:52 +0100

    fix

--- src/main/java/com/zimmerbell/photo_utils/Main.java
+++ src/main/java/com/zimmerbell/photo_utils/Main.java
@@ -17,21 +17,23 @@ public class Main {
 
 	public final static String OPT_OVERWRITE = "overwrite";
 
-	public final static String OPT_STAMP = "stamp";
 	public final static String OPT_RENAME = "rename";
-	public final static List<String> CHANGE_OPTIONS = Collections.unmodifiableList(Arrays.asList(new String[] { OPT_STAMP }));
+	public final static String OPT_STAMP = "stamp";
+	public final static String OPT_RESIZE = "resize";
+	public final static List<String> CHANGE_OPTIONS = Collections.unmodifiableList(Arrays.asList(new String[] { OPT_STAMP, OPT_RESIZE }));
 
 	public static void main(String[] args) throws Throwable {
 		Options options = new Options();
 
 		options.addOption(Option.builder("h").longOpt(OPT_HELP).build());
-		options.addOption(Option.builder("s").longOpt(OPT_SOURCE).argName("path").hasArg().required().build());
+		options.addOption(Option.builder("s").longOpt(OPT_SOURCE).argName("path").hasArg().build());
 		options.addOption(Option.builder("d").longOpt(OPT_DESTINATION).argName("path").hasArg().build());
 
 		options.addOption(Option.builder("o").longOpt(OPT_OVERWRITE).build());
 
-		options.addOption(Option.builder().longOpt(OPT_STAMP).build());
-		options.addOption(Option.builder().longOpt(OPT_RENAME).build());
+		options.addOption(Option.builder().longOpt(OPT_RENAME).desc("rename files according to their capture date").build());
+		options.addOption(Option.builder().longOpt(OPT_STAMP).desc("print capture date in image").build());
+		options.addOption(Option.builder().longOpt(OPT_RESIZE).desc("resize image").build());
 
 		CommandLine cl = new DefaultParser().parse(options, args);
 		if (cl.hasOption(OPT_HELP)) {
